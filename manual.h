681bc956c46628445f34e875e1217682e944b960
AjinkyaGhatol <31014537+AjinkyaGhatol@users.noreply.github.com>
2020-08-25 12:05:43 +0530

    Create manual.h

---
+++ manual.h
@@ -0,0 +1,6 @@
+void getManualFromNumber(int pairNumber)
+{
+    TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << "Got pair " << colorPair.ToString() << std::endl;
+}
