de8f62408e76a6ad2c41243c98c32b7dc8b38c12
AjinkyaGhatol <31014537+AjinkyaGhatol@users.noreply.github.com>
2020-08-25 12:06:18 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
 
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-
+    
+    getManualFromNumber(12);
     return 0;
 }
