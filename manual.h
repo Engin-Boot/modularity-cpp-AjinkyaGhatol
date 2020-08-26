4deda888057cc402c00efbe2c90cac0a6eb1ff8d
AjinkyaGhatol <31014537+AjinkyaGhatol@users.noreply.github.com>
2020-08-26 11:36:33 +0530

    Delete manual.h

--- manual.h
+++
@@ -1,6 +0,0 @@
-void getManualFromNumber(int pairNumber)
-{
-    TelCoColorCoder::ColorPair colorPair =
-        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-    std::cout << "Got pair " << colorPair.ToString() << std::endl;
-}
