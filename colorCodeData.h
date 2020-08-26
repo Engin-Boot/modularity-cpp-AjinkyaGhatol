bffae250e06215bbeec4a75baae6bc8e5b93d470
AjinkyaGhatol <31014537+AjinkyaGhatol@users.noreply.github.com>
2020-08-26 12:52:05 +0530

    Update colorCodeData.h

--- colorCodeData.h
+++ colorCodeData.h
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <assert.h>
-
-    enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+   enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
     enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
     const char* MajorColorNames[] = {
         "White", "Red", "Black", "Yellow", "Violet"
