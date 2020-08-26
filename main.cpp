8d714625939e2be418941e0121364c5581d96cd4
AjinkyaGhatol <31014537+AjinkyaGhatol@users.noreply.github.com>
2020-08-26 11:43:54 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
-#include "test.h"
-#include "manual.h"
+#include "testColorCode.h"
+#include<string>
+using namespace std;
+string ToString()
+{
+    string manual = "Pair no.\tMajor color\tMinor color\n";
+    int pairNumber = 1;
+    for (int i = 1; i <= numberOfMajorColors; i++)
+    {
+        for (int j = 1; j <= numberOfMinorColors; j++)
+        {
+            manual = manual + to_string(pairNumber) + "\t\t" + MajorColorNames[i - 1] + "\t\t" + MinorColorNames[j - 1] + "\n";
+            pairNumber++;
+        }
+    }
+    return manual;
+}
 int main() {
-    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
-    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
 
-    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
-    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-    
-    getManualFromNumber(12);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+    string manual = ToString();
+    //cout << manual;
     return 0;
 }
