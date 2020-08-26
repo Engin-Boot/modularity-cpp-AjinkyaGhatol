8db86a547caf0b88068ff97254dad385d9a99e39
AjinkyaGhatol <31014537+AjinkyaGhatol@users.noreply.github.com>
2020-08-26 11:54:59 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 string ToString()
 {
-    string manual = "Pair no.\tMajor color\tMinor color\n";
+                       
+    string manual = "-------------------------------------------------\n|Pair no.\t|Major color\t|Minor color\t|\n-------------------------------------------------\n";
     int pairNumber = 1;
     for (int i = 1; i <= numberOfMajorColors; i++)
     {
+        
         for (int j = 1; j <= numberOfMinorColors; j++)
         {
-            manual = manual + to_string(pairNumber) + "\t\t" + MajorColorNames[i - 1] + "\t\t" + MinorColorNames[j - 1] + "\n";
+            manual = manual + "|"+to_string(pairNumber) + "\t\t|" + MajorColorNames[i - 1] + "\t\t|" + MinorColorNames[j - 1] + "\t\t|\n";
             pairNumber++;
         }
+        manual = manual + "-------------------------------------------------\n";
     }
     return manual;
 }
@@ -22,6 +25,6 @@ int main() {
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
     string manual = ToString();
-    //cout << manual;
+    cout << manual;
     return 0;
 }
